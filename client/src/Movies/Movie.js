b97d2648acfa828e399a492061e3484c4bcfa717
karenjli <karenjy.li@gmail.com>
2019-08-20 18:11:29 -0500

    refactored MovieCard

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {Route, Link} from 'react-router-dom';
+import MovieCard from "./MovieCard";
 
 
 const Movie = (props) => {
@@ -36,31 +37,7 @@ const Movie = (props) => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-       
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {/* {(!stars)
-            ? <div>Loading Stars...</div> 
-            : stars.map(star => (
-                <div key={star} className="movie-star">
-                    {star}
-          </div>
-          ))} */}
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
+        <MovieCard key={movie.id} movie={movie} />
        
       <div className="save-button" onClick = {() => saveMovie(movie)}>Save</div>
     </div>
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const MovieCard = props => {
-  return;
+  
+   // const { title, director, metascore, stars } = movie;
+  return (
+    <Link to={`/movie/${props.movie.id}`}>
+      <div className="movie-card">
+        <h2>{props.movie.title}</h2>
+        <div className="movie-director">
+          Director: <em>{props.movie.director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{props.movie.metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        
+          <div className="movie-star">
+            {props.movie.stars.map (star => <div key={star}> {star} </div>)}
+           </div>
+        
+      </div>
+    </Link>
+  
+  );
 };
 
 export default MovieCard;
